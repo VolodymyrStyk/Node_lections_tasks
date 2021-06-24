280f51b490c15ecf55db6654c2310757bae2444a
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-06-24 12:50:24 +0300

    bag beed synchro

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const expressHbs = require('express-handlebars');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
+const dBPath = path.join(__dirname, 'data_base', 'users.txt');
 
 app.listen(3000, () => {
     console.log('App listen 3000');
@@ -19,14 +21,13 @@ app.engine('.hbs', expressHbs({
 }));
 
 app.get('/', (req, res) => {
-    res.render('index', {name: 'Volodymyr'});
+    res.render('login');
 });
 
-app.get('/ping', (req, res) => {
-    res.end('pong');
-});
-app.get('/', (req, res) => {
-    res.json('Main Page');
+app.post('/', async (req, res) => {
+    const response = await _wrUserToDataBase(req.body);
+    await console.log(response);
+    await res.json(response);
 });
 app.get('/register', (req, res) => {
     res.json('register');
@@ -38,3 +39,47 @@ app.get('/users', (req, res) => {
     res.json('users');
 });
 
+
+function _wrUserToDataBase(user) {
+    let response;
+    fs.readFile(dBPath, (err, data) => {
+        if (err) {
+            console.log('* -Can not read File: ', err);
+            return;
+        }
+        const users = (!data || data.toString() === '') ? _dataInit() : JSON.parse(data);
+        const isExist = _checkUser(users, user.login);
+        console.log('wr '+isExist);
+        if(!isExist){
+            fs.writeFile(dBPath, JSON.stringify(users), (err) => {
+                if (err) {
+                    console.log(57, err);
+                }
+            });
+            response = 'User EXIST NOK';
+        }
+        if(isExist){
+            response = 'Hello New User';
+        }
+    })
+    return response;
+}
+
+function _checkUser(users = [], newUserLogin) {
+    let response = true;
+    users.forEach(({login}) => {
+        if (login === newUserLogin) {
+            console.log('func - Exist');
+            response = false;
+        }
+    })
+    return response;
+}
+
+function _dataInit() {
+    return [{"login": "login", "password": "password"}];
+}
+
+app.get('/ping', (req, res) => {
+    res.end('pong');
+});
